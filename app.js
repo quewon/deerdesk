ea9a68ac6b92f368b7e1ab7f12addb1d87082de5
quewon <36914642+quewon@users.noreply.github.com>
2021-11-25 01:50:41 +0900

    omgomgomgomgomg
    server time

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const http = require('http');
 const app = express();
-const path = require('path');
-const router = express.Router();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static('/', router));
+app.use(express.static('/'));
 app.use('/lib/', express.static('lib'));
 app.use('/sounds/', express.static('sounds'));
 app.use('/images/', express.static('images'));
 app.use('/models/', express.static('models'));
+app.use('/node_modules/', express.static('node_modules'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(__dirname+'/index.html');
+});
+
+// https://stackoverflow.com/questions/41381444/websocket-connection-failed-error-during-websocket-handshake-unexpected-respon
+
+io.on('connection', (socket) => {
+  console.log('user connected!');
+
+  socket.on('greet', (data) => {
+    console.log(data);
+    socket.emit('respond', { message: 'sup, client.' });
+  });
+  socket.on('disconnect', function() {
+    console.log('user disconnected.');
+  });
 });
 
 const port = process.env.port || 3000;
-app.listen(port);
-console.log("live at http://localhost:"+port);
+server.listen(port);
+console.log("live at http://localhost:"+port+", probably");
