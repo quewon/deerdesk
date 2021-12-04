2cb5ad335b80015a00b1b301f818295689b61434
quewon <36914642+quewon@users.noreply.github.com>
2021-12-05 03:46:04 +0900

    .

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ io.on('connection', (socket) => {
 
 const port = process.env.port || 3000;
 server.listen(port, function() {
-  var addr = app.address();
+  var addr = server.address();
   console.log('app listening on http://' + addr.address + ':' + addr.port);
 });
